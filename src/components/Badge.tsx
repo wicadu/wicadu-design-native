33409bb3ae50c6566355439fb6cd4ce3f936ebb7
Joaquin Araujo <hola@joaquinaraujojs.com>
2020-05-09 21:38:37 -0400

    add badge component

---
+++ src/components/Badge.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import { StyleSheet, View, Text } from 'react-native'
+import { Colors } from '../constants'
+import PropTypes from 'prop-types'
+
+const propTypes = {
+  children: PropTypes.node.isRequired,
+  count: PropTypes.number,
+  onPress: PropTypes.func
+}
+
+interface Props {
+  children: PropTypes.ReactNodeLike
+  count?: number
+  onPress?: () => void
+}
+
+const defaultProps: Props = {
+  count: 0,
+  onPress: () => {}
+}
+
+function Badge ({ children, count, onPress }: Props) {
+  return (
+    <TouchableOpacity style={styles.container} onPress={onPress}>
+      <View style={styles.count}>
+        <Text style={styles.countNumber}>{count}</Text>
+      </View>
+      {children}
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: 50,
+    height: 50,
+    borderRadius: 50 / 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.third,
+    position: 'relative'
+  },
+  count: {
+    width: 25,
+    height: 25,
+    borderRadius: 25 / 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.primary,
+    position: 'absolute',
+    top: 0,
+    right: 0
+  },
+  countNumber: {
+    color: Colors.white
+  }
+})
+
+export default Badge
--- src/index.d.ts
+++ src/index.d.ts
@@ -1,10 +1,10 @@
-import Card from './components/Card'
 import Button from './UI/Button'
+import Badge from './components/Badge'
 import Colors from './constants/colors'
 import Fonts from './constants/fonts'
 
 export {
-  Card,
+  Badge,
   Button,
   Colors,
   Fonts
