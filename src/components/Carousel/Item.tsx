789a6f4a8a058798686333b736280f26763e68ec
Jonathan Araujo <j4asdev@gmail.com>
2020-05-11 00:30:19 -0400

    Add carousel and carouselItem components

---
+++ src/components/Carousel/Item.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { StyleSheet, Text, Image, TouchableOpacity } from 'react-native'
+import PropTypes, { InferProps } from 'prop-types'
+
+const propTypes = {
+  image: PropTypes.string,
+  name: PropTypes.string,
+  price: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number
+  ]),
+  onPress: PropTypes.func.isRequired
+}
+
+type Props = InferProps<typeof propTypes>
+
+function Item ({ image, name, price, onPress }: Props) {
+  return (
+    <TouchableOpacity style={styles.container} onPress={() => onPress()}>
+      { image && <Image source={{ uri: image }} style={styles.image} /> }
+      { name && <Text>{name}</Text> }
+      { price && <Text>${price}</Text> }
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    width: 160,
+  },
+  image: {
+    height: '70%',
+    width: 'auto',
+    borderRadius: 15
+  }
+})
+
+export default Item
--- src/index.d.ts
+++ src/index.d.ts
@@ -3,11 +3,13 @@ import Button from './components/Button'
 import InputText from './components/InputText'
 import Colors from './constants/colors'
 import Fonts from './constants/fonts'
+import Carousel from './components/Carousel/index'
 
 export {
   Badge,
   Button,
   InputText,
   Colors,
-  Fonts
+  Fonts,
+  Carousel
 }
