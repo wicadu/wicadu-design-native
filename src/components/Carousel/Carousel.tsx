20bd4986ac556e6447ea9ec1e04d0e53530dd9e3
Jonathan Araujo <j4asdev@gmail.com>
2020-05-12 00:43:26 -0400

    Add carousel and carousel.item components

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -4,6 +4,7 @@ import PropTypes, { InferProps } from 'prop-types'
 
 import Item from './Item'
 import { Fonts } from '../../constants'
+import { Button } from 'wcado-design-native'
 
 const propTypes = {
   children: PropTypes.node,
@@ -16,6 +17,12 @@ function Carousel ({ title, children }: Props) {
   return (
     <View>
       {title && <Text style={styles.title}>{title}</Text>}
+      <Button
+        title='See all'
+        className='primary-btn-inverse'
+        type='ghost'
+        onPress={() => {}}
+      />
       <ScrollView
         horizontal={true}
         contentContainerStyle={styles.itemList}
@@ -29,7 +36,8 @@ function Carousel ({ title, children }: Props) {
 
 const styles = StyleSheet.create({
   title: {
-    fontSize: Fonts.size22
+    fontSize: Fonts.size22,
+    fontWeight: 'bold'
   },
   itemList: {
     height: 260,
--- src/components/Carousel/Item.tsx
+++ src/components/Carousel/Item.tsx
@@ -1,25 +1,18 @@
 import React from 'react'
-import { StyleSheet, Text, Image, TouchableOpacity } from 'react-native'
+import { StyleSheet, TouchableOpacity } from 'react-native'
 import PropTypes, { InferProps } from 'prop-types'
 
 const propTypes = {
-  image: PropTypes.string,
-  name: PropTypes.string,
-  price: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number
-  ]),
-  onPress: PropTypes.func.isRequired
+  onPress: PropTypes.func.isRequired,
+  children: PropTypes.node
 }
 
 type Props = InferProps<typeof propTypes>
 
-function Item ({ image, name, price, onPress }: Props) {
+function Item ({ children, onPress }: Props) {
   return (
     <TouchableOpacity style={styles.container} onPress={() => onPress()}>
-      { image && <Image source={{ uri: image }} style={styles.image} /> }
-      { name && <Text>{name}</Text> }
-      { price && <Text>${price}</Text> }
+      {children}
     </TouchableOpacity>
   )
 }
@@ -28,11 +21,6 @@ const styles = StyleSheet.create({
   container: {
     padding: 10,
     width: 160,
-  },
-  image: {
-    height: '70%',
-    width: 'auto',
-    borderRadius: 15
   }
 })
 
