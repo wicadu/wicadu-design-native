91158e58c409b088d0b2d6e6f72f8e4a03c23e87
Jonathan Araujo <j4asdev@gmail.com>
2020-10-26 00:28:00 -0300

    Fix imports/exports

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,19 +1,22 @@
-import Button from './components/Button'
+// Atoms
+import TextInput from './atoms/TextInput'
+import Button from './atoms/Button'
+import IconContainer from './atoms/IconContainer'
+import Badge from './atoms/Badge'
+
+// Molecules
 import SearchBar from './components/SearchBar'
-import InputText from './components/InputText'
-import Badge from './components/Badge'
 import Carousel from './components/Carousel'
 import Slider from './components/Slider'
 import Price from './components/Price'
 import Count from './components/Count'
-import IconContainer from './components/IconContainer'
 import Colors from './constants/colors'
 import Fonts from './constants/fonts'
 
 export {
   Button,
   SearchBar,
-  InputText,
+  TextInput,
   Badge,
   Carousel,
   Slider,
