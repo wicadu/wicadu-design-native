844e3a3392a9762d3e12f3b6a6189e2e0d3e5ab2
Joaquin Araujo <hola@joaquinaraujojs.com>
2020-11-16 07:59:45 -0300

    recovery old button

--- src/atoms/Button.tsx
+++ src/atoms/Button.tsx
@@ -1,36 +1,31 @@
 import React from 'react'
 import { StyleSheet, View, Text, TouchableWithoutFeedback } from 'react-native'
+import Colors from '../constants/colors'
 import PropTypes, { InferProps } from 'prop-types'
 
-import Colors from '../constants/colors'
-
 const propTypes = {
+  title: PropTypes.string.isRequired,
   type: PropTypes.string,
   inverse: PropTypes.bool,
-  title: PropTypes.string,
-  bgColor: PropTypes.string,
-  usingTheme: PropTypes.bool,
-  height: PropTypes.number,
-  width: PropTypes.number
+  size: PropTypes.string,
+  onPress: PropTypes.func.isRequired,
+  id: PropTypes.string
 }
 
 interface DefaultProps {
+  type?: string,
+  size?: string,
+  inverse?: boolean,
   disabled?: boolean,
-  id: string
-  onPress?: () => void,
-  accessible: boolean
-} 
+  onPress?: () => void
+}
 
-const defaultProps: (DefaultProps & Props) = {
-  onPress: () => {},
-  disabled: false,
-  id: '',
-  accessible: true,
+const defaultProps: DefaultProps = {
+  type: 'primary',
+  size: 'large',
   inverse: false,
-  bgColor: 'primary',
-  usingTheme: true,
-  height: 55,
-  width: null
+  onPress: () => {},
+  disabled: false
 }
 
 type Props = InferProps<typeof propTypes>
@@ -38,10 +33,10 @@ type Props = InferProps<typeof propTypes>
 function Button(props: Props & DefaultProps) {
   const generatedStyles = styles(props)
 
-  const { title, onPress, disabled, id, accessible } = props
+  const { title, onPress, disabled } = props
 
   return (
-    <TouchableWithoutFeedback onPress={onPress} accessible={accessible} accessibilityLabel={id} disabled={disabled}>
+    <TouchableWithoutFeedback onPress={onPress}disabled={disabled}>
       <View style={generatedStyles.container}>
         <Text style={generatedStyles.text}>{title}</Text>
       </View>
@@ -49,33 +44,83 @@ function Button(props: Props & DefaultProps) {
   )
 }
 
-const styles = (props: (Props & DefaultProps)) => {
-  const { inverse, bgColor, height, width } = props
+const styles = ({ type, size, inverse }: DefaultProps) => {
+  const classType: string = `${type}${inverse ? '-inverse' : ''}`
 
-  const containerColors = inverse ? {
-    borderWidth: 1,
-    borderColor: Colors[bgColor] || Colors.primary,
-    backgroundColor: 'transparent'
-  } : {
-    backgroundColor: Colors[bgColor] || Colors.primary,
-    borderWidth: 0,
+  const defaultStylesContainer: object = {
+    borderRadius: 10,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 10
+  }
+
+  const defaultStylesText: object = {
+    fontFamily: 'Cabin_Regular',
+    fontSize: 16
+  }
+
+  const classes: object = {
+    container: {
+      'primary': {
+        ...defaultStylesContainer,
+        backgroundColor: Colors.primary,
+      },
+      'primary-inverse': {
+        ...defaultStylesContainer,
+        borderWidth: 1,
+        borderColor: Colors.primary
+      },
+      'ghost': {
+        ...defaultStylesContainer,
+        backgroundColor: Colors.darkGray
+      },
+      'ghost-inverse': {
+        ...defaultStylesContainer,
+        borderWidth: 1,
+        borderColor: Colors.darkGray
+      }
+    },
+    sizeContainer: {
+      'small': {
+        width: 100,
+        height: 30
+      },
+      'medium': {
+        width: 180,
+        height: 35
+      },
+      'large': {
+        height: 55
+      }
+    },
+    text: {
+      'primary': {
+        ...defaultStylesText,
+        color: Colors.white
+      },
+      'primary-inverse': {
+        ...defaultStylesText,
+        color: Colors.primary
+      },
+      'ghost': {
+        ...defaultStylesText,
+        color: Colors.white
+      },
+      'ghost-inverse': {
+        ...defaultStylesText,
+        color: Colors.darkGray
+      }
+    }
   }
 
   return StyleSheet.create({
     container: {
-      ...containerColors,
-      borderRadius: 15,
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      marginVertical: 10,
-      height: height,
-      width: width
+      ...classes.container[classType],
+      ...classes.sizeContainer[size]
     },
     text: {
-      fontSize: 16,
-      fontWeight: 'bold',
-      color: inverse ? Colors[bgColor] : 'white',
+      ...classes.text[classType]
     }
   })
 }
